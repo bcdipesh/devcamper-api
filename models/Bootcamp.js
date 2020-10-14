c35c1eb87256a29db13ed568eede75deeee45125
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-14 20:15:04 +0545

    fixed typos and method calls

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -114,11 +114,11 @@ BootcampSchema.pre("save", async function (next) {
   const loc = await geocoder.geocode(this.address);
   this.location = {
     type: "Point",
-    coordinates: [loc[0].longitude, loc[0] - lattitube],
+    coordinates: [loc[0].longitude, loc[0].latitude],
     formattedAddress: loc[0].formattedAddress,
     street: loc[0].streetName,
     city: loc[0].city,
-    state: loc[0].state,
+    state: loc[0].stateCode,
     zipcode: loc[0].zipcode,
     country: loc[0].countryCode,
   };
