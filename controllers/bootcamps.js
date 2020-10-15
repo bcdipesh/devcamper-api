ba60c092b43a9fac32b347d4a28f7546c5bca41b
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-15 20:14:08 +0545

    added Course model

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,7 +17,7 @@ bootcampController.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude from the query
-  const fieldsToExclude = ["select", "sort"];
+  const fieldsToExclude = ["select", "sort", "page", "limit"];
 
   // Loop over fieldsToExclude and delete them from reqQuery
   fieldsToExclude.forEach((param) => delete reqQuery[param]);
@@ -48,12 +48,39 @@ bootcampController.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort("-createdAt");
   }
 
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 100;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // Execute query
   const bootcamp = await query;
+
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
 
   res.status(200).json({
     success: true,
     count: bootcamp.length,
+    pagination,
     data: bootcamp,
   });
 });
