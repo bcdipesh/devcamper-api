10ef0ff2291b65d5c829af8403813820c523a01c
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-15 13:05:12 +0545

    added select and sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,14 +12,43 @@ const bootcampController = {};
 // @access      Public
 bootcampController.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
-  let queryStr = JSON.stringify(req.query);
+
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude from the query
+  const fieldsToExclude = ["select", "sort"];
+
+  // Loop over fieldsToExclude and delete them from reqQuery
+  fieldsToExclude.forEach((param) => delete reqQuery[param]);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators like $gt, $gte, $lte, etc.,
   queryStr = queryStr.replace(
     /\b(gt|gte|lt|lte|in)\b/g,
     (match) => `$${match}`
   );
 
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
 
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // Sort results
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  // Execute query
   const bootcamp = await query;
 
   res.status(200).json({
