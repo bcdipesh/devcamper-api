311a8da4a28c578a95fddb7a2e540bfc8dc3e4b6
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-18 20:33:36 +0545

    added methods to Course Model to calculate average cost while saving and deleting course data

--- models/Course.js
+++ models/Course.js
@@ -40,4 +40,36 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// Static method to get average of course tutions.
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Calculate average cost after save or remove
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
