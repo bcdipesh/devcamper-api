0c909499fabbc3bb9c4082eb874cb7ff24cc9545
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-29 19:19:20 +0545

    added secure property to cookie for production release

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,6 +56,10 @@ const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
+  if (process.env.NODE_ENV == "production") {
+    options.secure = true;
+  }
+
   const options = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
