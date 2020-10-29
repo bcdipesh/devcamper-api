5cf43c5ee550e36bab2f388efe26ef52745f1b6a
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-29 19:07:48 +0545

    added login methods and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const asyncHandler = require("../middleware/asyncHandler");
 
 const authController = {};
 
-// @description Get all bootcamps
+// @description Register new user
 // @route       GET /api/v1/auth/register
 // @access      Public
 authController.register = asyncHandler(async (req, res, next) => {
@@ -19,6 +19,40 @@ authController.register = asyncHandler(async (req, res, next) => {
     password,
     role,
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+// @description Login user
+// @route       POST /api/v1/auth/login
+// @access      Public
+authController.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check if user is in the db
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password is correct
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
   // Create token
   const token = user.getSignedJwtToken();
