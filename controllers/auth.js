8acc084818394919195677176c5d3e0ac0a15ea1
bc-dipesh <bcdipeshwork@gmail.com>
2020-10-31 23:08:12 +0545

    new feature added to update user details

--- controllers/auth.js
+++ controllers/auth.js
@@ -137,6 +137,43 @@ authController.resetPassword = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @description Update user details
+// @route       PUT /api/v1/auth/updateDetails
+// @access      Private
+authController.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @description Update user password
+// @route       PUT /api/v1/auth/updatePassword
+// @access      Private
+authController.updatePassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse("Password is incorrect", 401));
+  }
+
+  user.password = req.body.newPassword;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const { protect } = require("../middleware/auth");
@@ -16,6 +18,8 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/updateDetails", protect, updateDetails);
+router.put("/updatePassword", protect, updatePassword);
 router.get("/forgotPassword", forgotPassword);
 router.put("/resetPassword/:resetToken", resetPassword);
 
