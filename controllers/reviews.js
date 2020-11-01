3429536a990bc6b06241c73615b027dadf123c55
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-01 21:51:26 +0545

    added route to add review for a specific bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -43,3 +43,26 @@ exports.getReivew = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @description Add review
+// @route       POST /api/v1/bootcamps/:bootcampId/reviews
+// @access      Public
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: review,
+  });
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const express = require("express");
-const { getReviews, getReview } = require("../controllers/reviews");
+const { getReviews, getReview, addReview } = require("../controllers/reviews");
 
 const Review = require("../models/Review");
 
@@ -10,13 +10,16 @@ const router = express.Router({ mergeParams: true });
 const advancedResults = require("../middleware/advancedResults");
 const auth = require("../middleware/auth");
 
-router.route("/").get(
-  advancedResults(Review, {
-    path: "bootcamp",
-    select: "name description",
-  }),
-  getReviews
-);
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 
 router.route("/:id").get(getReview);
 
