1a87047974221bd121e4e421aa9f6bdcb66d5142
bc-dipesh <bcdipeshwork@gmail.com>
2020-11-01 21:34:47 +0545

    added get routes for reviews

---
+++ controllers/reviews.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Bootcamp = require("../models/Bootcamp");
+const Review = require("../models/Review");
+const ErrorResponse = require("../utils/ErrorResponse");
+const asyncHandler = require("../middleware/asyncHandler");
+
+// @description Get Reviews
+// @route       GET /api/v1/reviews
+// @route       GET /api/v1/bootcamps/:bootcampId/reviews
+// @access      Public
+exports.getReviews = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: reviews.length,
+      data: reviews,
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
+});
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,9 +18,11 @@ const { protect, authorize } = require("../middleware/auth");
 
 // Include other resource routers
 const courseRouter = require("./courses");
+const reviewRouter = require("./reviews");
 
 // Re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
+router.use("/:bootcampId/reviews", reviewRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
